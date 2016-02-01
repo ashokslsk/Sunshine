2fc2c3f2c35719a984b32325d38841a917750741
ashok.kumar <ashok.kumar@dunstconsulting.in>
2016-02-01 13:07:34 +0530

    After array set string elements

---
+++ app/src/main/java/com/ashokslsk/ashu_sunshine/MainActivity.java
@@ -0,0 +1,79 @@
+package com.ashokslsk.ashu_sunshine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //Fake mockdata
+
+            String[] forecastArray ={
+              "Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/40",
+                    "Wednesday - cloudy - 72/63",
+                    "Thursday - Asteroids - 75/65",
+                    "Friday - Heavy Rain - 65/56",
+                    "Saturday - HELP TRAPPED IN WEATHERSTATION - 60/51",
+                    "Sunday - Sunny - 80/68"
+            };
+
+            return rootView;
+        }
+    }
+
+}
